a2aaac05163fb2bc80798cdc3b663ea674145ff2
Nico <nmanograsso@gmail.com>
2022-09-12 20:54:40 -0300

    initial commit

---
+++ iOS-template-uikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-template-uikit
+//
+//  Created by nicolas.e.manograsso on 12/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
